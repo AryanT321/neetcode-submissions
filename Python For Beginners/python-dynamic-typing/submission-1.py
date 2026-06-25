15b680af34a4e613d38d4655f5c7c4c6e5e520d7
Aryan Tiwari <127074394+AryanT321@users.noreply.github.com>
2026-06-25 17:26:21 -0400

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable, variable = "hello", 20
+print(type(variable))
+variable = 202.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = ["hi","there"]
+print(type(variable))
