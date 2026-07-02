4e46c899f0508ef07b2d60ecefd4b365f16d141e
Aryan Tiwari <127074394+AryanT321@users.noreply.github.com>
2026-07-02 01:53:19 -0400

    Add: car-fleet - submission-9

---
+++ Data Structures & Algorithms/car-fleet/submission-9.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        my_stack = []
+        my_list = []
+        for i in range(len(position)):
+            my_list.append((position[i], speed[i]))
+        my_list.sort(key=lambda x: x[0])
+
+        print(my_list)
+
+        for i in my_list:
+            time = (target - i[0])/i[1]
+            while my_stack and my_stack[-1] <= time:
+                my_stack.pop()
+            my_stack.append(time)
+
+        print(my_stack)
+        return len(my_stack)
